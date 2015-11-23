866324643dea2644aafba357ff9cfadbffc19912
jchildren <john.a.children@gmail.com>
2015-11-23 13:07:38 -0800

    Begins implementation of urls and views

--- GroupThink/wiki/views.py
+++ GroupThink/wiki/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Revision
+
+def index(request):
+    latest_revisions_list = Revision.objects.order_by('-date')[:5]
+    output = ', '.join([r.text.body for r in latest_revisions_list])
+    return HttpResponse(output)
+    
+def page(request, page_id):
+     return HttpResponse("You're looking at page %s." % page_id)
+     
+def edit(request, page_id):
+    return HttpResponse("You're editing page %s." % page_id)
+
+def revisions(request, page_id):
+    return HttpResponse("You're looking at page %s revisions." % page_id)
